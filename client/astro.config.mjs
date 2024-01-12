73023da1176e389bf4848dead2514f4318cf649d
serkanisyapan <serkanisyapan1@hotmail.com>
2024-01-12 20:53:27 +0300

    vercel changes

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercelServerless from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react()],
-  adapter: vercel(),
+  adapter: vercelServerless(),
 });
