bde6011eb67634c104e1b8326af739243c2da009
serkanisyapan <serkanisyapan1@hotmail.com>
2024-01-13 22:32:48 +0300

    try netlify

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercelServerless from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react()],
-  adapter: vercelServerless(),
+  adapter: netlify(),
 });
